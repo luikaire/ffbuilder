570aa9341bccf38fe41d199e96a66599181780c2
Your Name <you@example.com>
2024-06-08 18:25:58 -0400

    auto

---
+++ scripts.d/50-vvenc.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/fraunhoferhhi/vvenc"
+
+ffbuild_enabled() {
+    return 0
+}
+
+fixarm64=()
+
+ffbuild_dockerbuild() {
+
+    for patch in /patches/*.patch; do
+        echo "Applying $patch"
+        git am < "$patch"
+    done
+    mkdir build && cd build
+
+    
+    if [[ $TARGET == *arm64 ]]; then
+    fixarm64=(
+        -DVVENC_ENABLE_X86_SIMD=OFF 
+        -DVVENC_ENABLE_ARM_SIMD=OFF
+    )
+    fi
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" "${fixarm64[@]}" ..
+    make -j$(nproc)
+    make install
+}
+
+ffbuild_configure() {
+    echo --enable-libvvenc
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libvvenc
+}
